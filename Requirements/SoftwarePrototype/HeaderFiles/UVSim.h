331eea01ba6350c4cd25b47a094ef79bc4e5e5ce
BryanCE <45415038+BryanCE@users.noreply.github.com>
2021-02-02 01:19:03 -0700

    Update UVSim.h

--- Requirements/SoftwarePrototype/HeaderFiles/UVSim.h
+++ Requirements/SoftwarePrototype/HeaderFiles/UVSim.h
@@ -14,6 +14,26 @@ protected:
 
 	Memory mem;
 	Accumulator acmltr;
+	
+	
+	//Some generic functions to parse an integer from the string
+        //credit to stack overflow help
+	template<typename T>
+	std::string ToString(const T& v)
+	{
+		std::ostringstream ss;
+		ss << v;
+		return ss.str();
+	}
+
+	template<typename T>
+	T FromString(const std::string& str)
+	{
+		std::istringstream ss(str);
+		T ret;
+		ss >> ret;
+		return ret;
+	}
 
 };
 
